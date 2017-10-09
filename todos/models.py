d217a6c072c31b2dbe48d15698a09555a42b550e
Norly <canariasnorly@gmail.com>
2017-10-09 23:06:39 +0800

    - Adds index route (renders string for now) - Adds Todo model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Todo(models.Model):
+    title       = models.CharField(max_length=80)
+    description = models.CharField(max_length=300)
+    is_done     = models.BooleanField(default=False)
+    due_date    = models.DateTimeField('date due_date')
