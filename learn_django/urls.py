f13674734e28d12a449eb1554b6a0a4f99488e5c
Norly <canariasnorly@gmail.com>
2017-10-14 11:00:42 +0800

    Enables deleting Todo
    Also:
    - Separates list of finished and unfinished todos
    - Used partial for rendering each todo

--- learn_django/urls.py
+++ learn_django/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     url(r'^finish_todo/(\d+)/$', views.finish, name='todo_finish'),
     url(r'^unfinish_todo/(\d+)/$', views.unfinish, name='todo_unfinish'),
     url(r'^create_todo/', views.create, name='todo_create'),
+    url(r'^destroy_todo/(\d+)/$', views.destroy, name='todo_destroy'),
     url(r'^admin/', admin.site.urls),
 ]
--- todos/views.py
+++ todos/views.py
@@ -5,8 +5,9 @@ from django.utils import timezone
 from IPython import embed
 
 def index(request):
-    todos = Todo.objects.filter(due_date__date=timezone.datetime.today())
-    context = {'todos': todos}
+    unfinished = _todos_today().filter(is_done=False)
+    finished   = _todos_today().filter(is_done=True)
+    context    = {'unfinished_todos': unfinished, 'finished_todos': finished}
     return render(request, 'index.html', context)
 
 def finish(request, id):
@@ -24,9 +25,17 @@ def create(request):
                 description=request.POST['description'],
                 due_date=timezone.now().date())
     todo.save()
+    return redirect('todos_index')
 
+def destroy(request, id):
+    todo = _get_todo(id)
+    todo.delete()
     return redirect('todos_index')
 
 def _get_todo(id):
     todo = Todo.objects.filter(id=id)
     return todo
+
+def _todos_today():
+    todos = Todo.objects.filter(due_date__date=timezone.datetime.today())
+    return todos
